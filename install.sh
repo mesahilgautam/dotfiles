cea7e7794dfa4ff1a92134bc7af63f5924d3fd77
Sahil <me.sahilgautam@gmail.com>
2023-11-20 22:15:39 +0530

    works now

--- install.sh
+++ install.sh
@@ -119,3 +119,7 @@ function installYay() {
 command -v yay || installYay
 packageInstall
 setup
+
+# for powerlevel10k
+#git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+#echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc

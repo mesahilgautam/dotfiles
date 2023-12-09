f83cb0952eac2268a520270294bf38a6e5c183c4
Sahil <me.sahilgautam@gmail.com>
2023-12-09 22:41:16 +0530

    Works, testing on a vm, also for myvim setup

--- myvim/init.lua
+++ myvim/init.lua
@@ -75,20 +75,19 @@ vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 -- https://github.com/folke/lazy.nvim to read more about lazy nvim and various options
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- treesitter
 -- comment on gc
--- vim-maximizer
 -- "jghauser/follow-md-links.nvim",
 -- markdown preview
 -- toggleterm
@@ -96,62 +95,76 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-	{ 
+    { 
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000 
     },
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		--		config = function()
-		--			require("nvim-tree").setup {}
-		--		end,
-	},
-	{
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        --		config = function()
+        --			require("nvim-tree").setup {}
+        --		end,
+    },
+
+    {
         "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} 
     },
-	{
+
+    {
         "christoomey/vim-tmux-navigator"
     },
-	{
+
+    {
         "ThePrimeagen/harpoon",
-		dependencies = {"nvim-lua/plenary.nvim"},
-	},
-	{
-		'windwp/nvim-autopairs',
-		event = "InsertEnter",
-		opts = {} -- this is equalent to setup({}) function
-	},
-	{
+        dependencies = {"nvim-lua/plenary.nvim"},
+    },
+
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        opts = {} -- this is equalent to setup({}) function
+    },
+
+    {
         "norcalli/nvim-colorizer.lua"
     },
-	{
+
+    {
         'akinsho/toggleterm.nvim', version = "*", config = true
     },
-	{
+
+    {
         'tpope/vim-commentary'
     },
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function() vim.fn["mkdp#util#install"]() end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ":TSUpdate",
         config = true,
-	},
+    },
+
 })
 
 -- ==========================================================================================================
@@ -165,18 +178,18 @@ require("lazy").setup({
 -- ==========================================================================================================
 
 require("catppuccin").setup({
-	flavour = "mocha", -- latte, frappe, macchiato, mocha
-	background = { -- :h background
-	light = "latte",
-	dark = "mocha",
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+    light = "latte",
+    dark = "mocha",
 },
 transparent_background = true, -- disables setting the background color.
 show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
 dim_inactive = {
-	enabled = false, -- dims the background color of inactive window
-	shade = "dark",
-	percentage = 0.15, -- percentage of the shade to apply to the inactive window
+    enabled = false, -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.15, -- percentage of the shade to apply to the inactive window
 },
 no_italic = false, -- Force no italic
 no_bold = false, -- Force no bold
@@ -194,21 +207,21 @@ booleans = {},
 properties = {},
 types = {},
 operators = {},
-	},
-	color_overrides = {},
-	custom_highlights = {},
-	integrations = {
-		cmp = true,
-		gitsigns = true,
-		nvimtree = true,
-		treesitter = true,
-		notify = false,
-		mini = {
-			enabled = true,
-			indentscope_color = "",
-		},
-		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-	},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+            enabled = true,
+            indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
 })
 
 -- setup must be called before loading
@@ -216,34 +229,34 @@ vim.cmd.colorscheme "catppuccin"
 
 -- To be changed Later (size, orientation etc)
 require('telescope').setup{
-	defaults = {
-		-- Default configuration for telescope goes here:
-		-- config_key = value,
-		mappings = {
-			i = {
-				-- map actions.which_key to <C-h> (default: <C-/>)
-				-- actions.which_key shows the mappings for your picker,
-				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
-				["<C-h>"] = "which_key"
-			}
-		}
-	},
-	pickers = {
-		-- Default configuration for builtin pickers goes here:
-		-- picker_name = {
-		--   picker_config_key = value,
-		--   ...
-		-- }
-		-- Now the picker_config_key will be applied every time you call this
-		-- builtin picker
-	},
-	extensions = {
-		-- Your extension configuration goes here:
-		-- extension_name = {
-		--   extension_config_key = value,
-		-- }
-		-- please take a look at the readme of the extension you want to configure
-	}
+    defaults = {
+        -- Default configuration for telescope goes here:
+        -- config_key = value,
+        mappings = {
+            i = {
+                -- map actions.which_key to <C-h> (default: <C-/>)
+                -- actions.which_key shows the mappings for your picker,
+                -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                ["<C-h>"] = "which_key"
+            }
+        }
+    },
+    pickers = {
+        -- Default configuration for builtin pickers goes here:
+        -- picker_name = {
+        --   picker_config_key = value,
+        --   ...
+        -- }
+        -- Now the picker_config_key will be applied every time you call this
+        -- builtin picker
+    },
+    extensions = {
+        -- Your extension configuration goes here:
+        -- extension_name = {
+        --   extension_config_key = value,
+        -- }
+        -- please take a look at the readme of the extension you want to configure
+    }
 }
 
 
@@ -251,71 +264,71 @@ require('telescope').setup{
 
 -- OR setup with some options
 require("nvim-tree").setup({
-	--  sort = {
-	--    sorter = "case_sensitive",
-	--  },
-	view = {
-		width = 30,
-	},
-	--  renderer = {
-	--    group_empty = true,
-	--  },
-	--  filters = {
-	--    dotfiles = true,
-	--  },
+    --  sort = {
+    --    sorter = "case_sensitive",
+    --  },
+    view = {
+        width = 30,
+    },
+    --  renderer = {
+    --    group_empty = true,
+    --  },
+    --  filters = {
+    --    dotfiles = true,
+    --  },
 })
 
 
 require("ibl").setup {
-	indent = { char = "│" },
-	scope = { enabled = false },
+    indent = { char = "│" },
+    scope = { enabled = false },
 }
 
 -- more customization options are available, if needed
 require 'colorizer'.setup {
-	'css';
-	'javascript';
-	'markdown';
-	html = {
-		mode = 'foreground';
-	},
+    'css';
+    'javascript';
+    'markdown';
+    html = {
+        mode = 'foreground';
+    },
 }
 
 
 
 require("toggleterm").setup({
-	size = 10,
-	open_mapping = [[<c-\>]],
-	hide_numbers = true,
-	shade_filetypes = {},
-	shade_terminals = true,
-	shading_factor = 2,
-	start_in_insert = true,
-	insert_mappings = true,
-	persist_size = true,
-	direction = "float",
-	close_on_exit = true,
-	shell = vim.o.shell,
-	float_opts = {
-		border = "curved",
-		winblend = 0,
-		highlights = {
-			border = "Normal",
-			background = "Normal",
-		},
-	},
+    size = 10,
+    open_mapping = [[<c-\>]],
+    hide_numbers = true,
+    shade_filetypes = {},
+    shade_terminals = true,
+    shading_factor = 2,
+    start_in_insert = true,
+    insert_mappings = true,
+    persist_size = true,
+    direction = "float",
+    close_on_exit = true,
+    shell = vim.o.shell,
+    float_opts = {
+        border = "curved",
+        winblend = 0,
+        highlights = {
+            border = "Normal",
+            background = "Normal",
+        },
+    },
 })
 
 -- this part makes it possible to switch between normal and insert modes in the terminal
 -- Why and how,I don't know, but will explore it in future.
 function _G.set_terminal_keymaps()
-	local opts = { noremap = true }
-	vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
-	vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+    local opts = { noremap = true }
+    vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+    vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
 end
 vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()") -- all thanks to neovim-from-scratch :)
 
